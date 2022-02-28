8f629252810db2d26b0b20efb8a8fa3e192865df
Pavel Brun <pavel.brun@victorinsurance.com>
2022-02-27 22:38:44 -0500

    Advent of Code Year 2015, Day 8

--- CommonAlgorithms/StringUtils.cs
+++ CommonAlgorithms/StringUtils.cs
@@ -10,5 +10,15 @@ namespace CommonAlgorithms
         {
             return (line.ToCharArray().Count(x => x == value));
         }
+
+        public static IEnumerable<int> AllIndexesOf(string str, string searchString)
+        {
+            int minIndex = str.IndexOf(searchString);
+            while (minIndex != -1)
+            {
+                yield return minIndex;
+                minIndex = str.IndexOf(searchString, minIndex + searchString.Length);
+            }
+        }
     }
 }
