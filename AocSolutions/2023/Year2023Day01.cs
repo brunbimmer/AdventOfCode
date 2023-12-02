b469626884ddb137459e15fcf09222a0fc74172d
brunbimmer <pavel.78.brun@gmail.com>
2023-12-02 18:32:33 -0500

    Advent of Code - 2023 Day 1 and 2

---
+++ AocSolutions/2023/Year2023Day01.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventFileIO;
+using Common;
+using LINQPad.Extensibility.DataContext;
+
+namespace AdventOfCode
+{
+    [AdventOfCode(Year = 2023, Day = 1)]
+    public class Year2023Day01: IAdventOfCode
+    {
+        private int _Year;
+        private int _Day;
+        private string _OverrideFile;
+
+        public Stopwatch SW { get; set; }
+
+        List<List<int>> calorieCollection = new List<List<int>>();
+
+        public Year2023Day01()
+        {
+            //Get Attributes
+            AdventOfCodeAttribute ca = (AdventOfCodeAttribute)Attribute.GetCustomAttribute(GetType(), typeof(AdventOfCodeAttribute));
+
+            _Year = ca.Year;
+            _Day = ca.Day;
+            _OverrideFile = ca.OverrideTestFile;
+
+            SW = new Stopwatch();
+        }
+
+        public void GetSolution(string path, bool trackTime = false)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine($"Launching Puzzle for Dec. {_Day}, {_Year}");
+            Console.WriteLine("===========================================");
+
+            //Build BasePath and retrieve input. 
+ 
+            string file = FileIOHelper.getInstance().InitFileInput(_Year, _Day, _OverrideFile ?? path);
+
+            string[] lines = FileIOHelper.getInstance().ReadDataAsLines(file);
+           
+            SW.Start();
+
+
+            int sum = CalculateSum(lines);
+
+            SW.Stop();
+
+            Console.WriteLine($"  Part 1: Calibration Value of input: {sum}");
+            Console.WriteLine("   Execution Time: {0}", StopwatchUtil.getInstance().GetTimestamp(SW));
+            
+            SW.Restart();
+
+            int trueSum = CalculateSumWithWords(lines);
+            
+            SW.Stop();
+
+            Console.WriteLine($"  Part 2: True calibration value: {trueSum}");
+            Console.WriteLine("   Execution Time: {0}", StopwatchUtil.getInstance().GetTimestamp(SW));
+
+
+        }
+
+        public int CalculateSum(string[] lines)
+        {
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                var digits = line.Where(char.IsDigit).ToArray();
+
+                if (digits.Length > 0)
+                {
+                    char firstDigit = digits.First();
+                    char lastDigit = digits.Last();
+                    sum += Convert.ToInt32($"{firstDigit}{lastDigit}");
+                }
+            }
+            return sum;
+        }
+
+        public int CalculateSumWithWords(string[] lines)
+        {
+            string pattern = @"(?=(one|two|three|four|five|six|seven|eight|nine))";
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                var digits = line.Where(char.IsDigit).ToArray();
+
+                char firstDigit;
+                char lastDigit;
+
+                if (digits.Length > 0)
+                {
+                    firstDigit = digits.First();
+                    lastDigit = digits.Last();
+                }
+                else
+                {
+                    firstDigit = lastDigit = ' ';
+                }
+
+                MatchCollection matches = Regex.Matches(line, pattern);
+
+                if (matches.Count > 0)
+                {
+
+                    int firstDigitPosition = line.IndexOfAny("0123456789".ToCharArray());
+                    int lastDigitPosition = line.LastIndexOfAny("0123456789".ToCharArray());
+
+
+                    if (matches.Count == 1)
+                    {
+                        string word = matches[0].Groups[1].Value;
+
+                        int wordPosition = line.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+
+                        if (firstDigitPosition == -1 && lastDigitPosition == -1)
+                            sum += Convert.ToInt32($"{GetValue(word)}{GetValue(word)}");
+                        else if (wordPosition < firstDigitPosition)
+                            sum += Convert.ToInt32($"{GetValue(word)}{lastDigit}");
+                        else if (wordPosition > lastDigitPosition)
+                            sum += Convert.ToInt32($"{firstDigit}{GetValue(word)}");
+                        else
+                            sum += Convert.ToInt32($"{firstDigit}{lastDigit}");
+                    }
+                    else
+                    {
+                        string firstWord = matches.First().Groups[1].Value;
+                        string lastWord = matches.Last().Groups[1].Value;
+
+                        int firstWordPosition = line.IndexOf(firstWord, StringComparison.OrdinalIgnoreCase);
+                        int lastWordPosition = line.LastIndexOf(lastWord, StringComparison.OrdinalIgnoreCase);
+
+                        if (   (firstDigitPosition == -1 && lastDigitPosition == -1)
+                            || (firstWordPosition < firstDigitPosition && lastWordPosition > lastDigitPosition))
+                            sum += Convert.ToInt32($"{GetValue(firstWord)}{GetValue(lastWord)}");
+                        else if (firstWordPosition < firstDigitPosition && lastDigitPosition > lastWordPosition)
+                            sum += Convert.ToInt32($"{GetValue(firstWord)}{lastDigit}");
+                        else if (firstDigitPosition < firstWordPosition && lastWordPosition > lastDigitPosition)
+                            sum += Convert.ToInt32($"{firstDigit}{GetValue(lastWord)}");
+                        else
+                            sum += Convert.ToInt32($"{firstDigit}{lastDigit}");
+                    }
+                }
+                else
+                {
+                    sum += Convert.ToInt32($"{firstDigit}{lastDigit}");
+                }
+            }
+            return sum;
+        }
+
+        public int GetValue(string input)
+        {
+            return input switch
+            {
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                "six" => 6,
+                "seven" => 7,
+                "eight" => 8,
+                "nine" => 9,
+                _ => 0,
+            };
+        }
+    }
+}
---
+++ AocSolutions/2023/Year2023Day02.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventFileIO;
+using Common;
+using LINQPad.Extensibility.DataContext;
+using Microsoft.Extensions.Primitives;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+    [AdventOfCode(Year = 2023, Day = 2)]
+    public class Year2023Day02: IAdventOfCode
+    {
+        private int _Year;
+        private int _Day;
+        private string _OverrideFile;
+
+        public Stopwatch SW { get; set; }
+
+        List<List<int>> calorieCollection = new List<List<int>>();
+
+        public Year2023Day02()
+        {
+            //Get Attributes
+            AdventOfCodeAttribute ca = (AdventOfCodeAttribute)Attribute.GetCustomAttribute(GetType(), typeof(AdventOfCodeAttribute));
+
+            _Year = ca.Year;
+            _Day = ca.Day;
+            _OverrideFile = ca.OverrideTestFile;
+
+            SW = new Stopwatch();
+        }
+
+        public void GetSolution(string path, bool trackTime = false)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine($"Launching Puzzle for Dec. {_Day}, {_Year}");
+            Console.WriteLine("===========================================");
+
+            //Build BasePath and retrieve input. 
+ 
+            string file = FileIOHelper.getInstance().InitFileInput(_Year, _Day, _OverrideFile ?? path);
+
+            string[] lines = FileIOHelper.getInstance().ReadDataAsLines(file);
+           
+            SW.Start();
+
+
+            int sumofIds = FindPossibleGames(lines);
+
+            SW.Stop();
+
+            Console.WriteLine($"  Part 1: Some of the IDs of possible games: {sumofIds}");
+            Console.WriteLine("   Execution Time: {0}", StopwatchUtil.getInstance().GetTimestamp(SW));
+            
+            SW.Restart();
+
+            int powerSetSum = FindPowerSetTotal(lines);
+            
+            SW.Stop();
+
+            Console.WriteLine($"  Part 2: Power Set Sum: {powerSetSum}");
+            Console.WriteLine("   Execution Time: {0}", StopwatchUtil.getInstance().GetTimestamp(SW));
+
+
+        }
+
+        public int FindPossibleGames(string[] lines)
+        {
+            int redCubesMax = 12;
+            int greenCubesMax = 13;
+            int blueClubesMax = 14;
+
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                string[] gameplay = line.Split(":");
+
+                int gameID = Convert.ToInt32(gameplay[0].Split(" ")[1]);
+
+
+                string[] sets = gameplay[1].Split(";");
+
+                bool possible = true;
+
+                foreach (string s in sets)
+                {
+                    string[] colorValuePairs = s.Split(",");
+
+                    foreach (string colorValuePair in colorValuePairs)
+                    {
+                        string[] parts = colorValuePair.Trim().Split(" ");
+
+                        string color = parts[1];
+                        int value = int.Parse(parts[0]);
+
+                        if (color == "red" && value > redCubesMax)
+                            possible = false;
+
+                        if (color == "green" && value > greenCubesMax)
+                            possible = false;
+
+                        if (color == "blue" && value > blueClubesMax)
+                            possible = false;
+
+                    }
+                }
+
+                if (possible)
+                    sum += gameID;
+            }
+            return sum;
+        }
+
+        public int FindPowerSetTotal(string[] lines)
+        {
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                string[] gameplay = line.Split(":");
+
+                string[] sets = gameplay[1].Split(";");
+
+                int blueVal = 0;
+                int greenVal = 0;
+                int redVal = 0;
+
+                foreach (string s in sets)
+                {
+
+                    string[] colorValuePairs = s.Split(",");
+
+                    foreach (string colorValuePair in colorValuePairs)
+                    {
+                        string[] parts = colorValuePair.Trim().Split(" ");
+
+                        string color = parts[1];
+                        int value = int.Parse(parts[0]);
+
+                        if (color == "red" && value > blueVal)
+                            blueVal = value;
+
+                        if (color == "green" && value > greenVal)
+                            greenVal = value;
+
+                        if (color == "blue" && value > redVal)
+                            redVal = value;
+
+                    }
+                }
+
+                sum += (redVal * blueVal * greenVal);
+            }
+            return sum;
+        }
+    }
+}
